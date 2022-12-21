418640ea0d366a88c961dbb47725a02f06aba339
VaasuAnnan <117959586+VaasuAnnan@users.noreply.github.com>
2022-12-21 23:37:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ except Exception as api_id: print(f"⚠️ Api Hash Invalid {api_hash}")
 try: bot_token = os.environ.get("bot_token", None)
 except Exception as bot_token: print(f"⚠️ Bot Token Invalid {bot_token}")
 try: custom_caption = os.environ.get("custom_caption", "<b>{file_name}</b>
-
 <b>♻️ @MSM_Updates | @MSMchat</b>")
 except Exception as custom_caption: print(f"⚠️ Custom Caption Invalid {custom_caption}")
 
